eb4a4e6e9c6331acb8986de66da682b390e1124d
Devananda van der Veen <devananda.vdv@gmail.com>
2016-07-19 16:21:43 -0700

    Rename project from "local" to "demo"

--- register.sh
+++ register.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 export OS_IDENTITY_API_VERSION=3
-export OS_PROJECT_NAME=local
+export OS_PROJECT_NAME=demo
 export OS_DOMAIN=default
 export OS_USERNAME=admin 
 export OS_PASSWORD=admin_pass 
@@ -15,17 +15,17 @@ openstack endpoint create -f value baremetal internal http://127.0.0.1:6385/
 # this is to fix a bug in openstackclient
 openstack endpoint create -f value baremetal public http://127.0.0.1:6385/
 
-openstack user create --domain default --project local --project-domain default --password password ironic
-openstack role add --user-domain default --project-domain default --project local --user ironic admin
+openstack user create --domain default --project demo --project-domain default --password password ironic
+openstack role add --user-domain default --project-domain default --project demo --user ironic admin
 
 openstack role create baremetal_admin
 openstack role create baremetal_observer
 
-openstack user create --domain default --project local --project-domain default --password password player_one
-openstack role add --user-domain default --project-domain default --project local --user player_one baremetal_admin
+openstack user create --domain default --project demo --project-domain default --password password player_one
+openstack role add --user-domain default --project-domain default --project demo --user player_one baremetal_admin
 
-openstack user create --domain default --project local --project-domain default --password password player_two
-openstack role add --user-domain default --project-domain default --project local --user player_two baremetal_observer
+openstack user create --domain default --project demo --project-domain default --password password player_two
+openstack role add --user-domain default --project-domain default --project demo --user player_two baremetal_observer
 
 if [[ ${0%$(basename $0)} == '/etc/init.d/' ]]; then
     rm $0
